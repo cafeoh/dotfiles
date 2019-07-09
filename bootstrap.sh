0c7d886c9b21ee14dd5bdb72ae0c44396fd8a0ea
Cafe <cogney.maxime@gmail.com>
2019-07-09 14:21:12 +0200

    Add bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PREV=`pwd`
+
+sudo apt-get update
+sudo apt-get upgrade
+
+
+cp -rf .* ..
+
+sudo apt-get install git vim exuberant-ctags ack
+
+##### i3-gaps #####
+sudo apt-get install i3
+cd ~
+git clone https://github.com/maestrogerardo/i3-gaps-deb
+cd i3-gaps-deb
+./i3-gaps-deb
+cd $PREV
+cp -rf .config ../
+
+
+####   zsh   #####
+sudo apt-get install zsh curl
+sudo chsh -s `which zsh`
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -rf ./oh-my-zsh.sh ../.oh-my-zsh
